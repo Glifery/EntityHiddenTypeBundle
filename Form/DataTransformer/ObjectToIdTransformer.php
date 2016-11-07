d2d830a79f1e93124ebaabd9fd1ddeb4a2f72c66
Tomasz Kuśka <tomasz.kuska@gmail.com>
2016-11-07 22:58:09 +0100

    Added support for multiple entities

--- Form/DataTransformer/ObjectToIdTransformer.php
+++ Form/DataTransformer/ObjectToIdTransformer.php
@@ -10,8 +10,8 @@ use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\Exception\InvalidConfigurationException;
 use Symfony\Component\Form\Exception\TransformationFailedException;
 
-class ObjectToIdTransformer implements DataTransformerInterface
-{
+class ObjectToIdTransformer implements DataTransformerInterface {
+
     /**
      * @var string
      */
@@ -31,58 +31,87 @@ class ObjectToIdTransformer implements DataTransformerInterface
     protected $repository;
 
     /**
+     * @var boolean
+     */
+    protected $multiple;
+
+    /**
      * @param ManagerRegistry $registry
      * @param string $class
      * @param string $property
      */
-    public function __construct(ManagerRegistry $registry, $om, $class, $property)
-    {
+    public function __construct(ManagerRegistry $registry, $om, $class, $property, $multiple = false) {
         $this->class = $class;
         $this->property = $property;
         $this->em = $this->getObjectManager($registry, $om);
         $this->repository = $this->getObjectRepository($this->em, $this->class);
+        $this->multiple = $multiple;
     }
 
     /**
      * @param mixed $entity
      * @return mixed|null
      */
-    public function transform($entity)
-    {
+    public function transform($entity) {
         if (null === $entity) {
             return null;
         }
 
+        $methodName = 'get' . ucfirst($this->property);
         $className = $this->repository->getClassName();
-        if (!$entity instanceof $className) {
-            throw new TransformationFailedException(sprintf('Object must be instance of %s, instance of %s has given.', $className, get_class($entity)));
+        if (!$this->multiple) {
+            if (!$entity instanceof $className) {
+                throw new TransformationFailedException(sprintf('Object must be instance of %s, instance of %s has given.', $className, get_class($entity)));
+            }
+
+            if (!method_exists($entity, $methodName)) {
+                throw new InvalidConfigurationException(sprintf('There is no getter for property "%s" in class "%s".', $this->property, $this->class));
+            }
+
+            return $entity->{$methodName}();
         }
 
-        $methodName = 'get'.ucfirst($this->property);
-        if (!method_exists($entity, $methodName)) {
-            throw new InvalidConfigurationException(sprintf('There is no getter for property "%s" in class "%s".', $this->property, $this->class));
+        $result = array();
+        foreach ($entity as $object) {
+            if (!$object instanceof $className) {
+                throw new TransformationFailedException(sprintf('Collection must contain instance of %s, instance of %s has given.', $className, get_class($entity)));
+            }
+
+            if (!method_exists($object, $methodName)) {
+                throw new InvalidConfigurationException(sprintf('There is no getter for property "%s" in class "%s".', $this->property, $this->class));
+            }
+
+            $result[] = $object->{$methodName}();
         }
 
-        return $entity->{$methodName}();
+        return implode(',', $result);
     }
 
     /**
      * @param mixed $id
      * @return mixed|null|object
      */
-    public function reverseTransform($id)
-    {
+    public function reverseTransform($id) {
         if (!$id) {
             return null;
         }
 
-        $entity = $this->repository->findOneBy(array($this->property => $id));
+        if (!$this->multiple) {
+            $entity = $this->repository->findOneBy(array($this->property => $id));
 
-        if (null === $entity) {
-            throw new TransformationFailedException(sprintf('Can\'t find entity of class "%s" with property "%s" = "%s".', $this->class, $this->property, $id));
+            if (null === $entity) {
+                throw new TransformationFailedException(sprintf('Can\'t find entity of class "%s" with property "%s" = "%s".', $this->class, $this->property, $id));
+            }
+
+            return $entity;
+            
+        } else{
+            $ids = explode(',', $id);
+
+            $entities = $this->repository->findBy(array($this->property => $ids));            
+
+            return $entities;
         }
-
-        return $entity;
     }
 
     /**
@@ -95,7 +124,7 @@ class ObjectToIdTransformer implements DataTransformerInterface
             return $omName;
         }
 
-        $omName = (string)$omName;
+        $omName = (string) $omName;
         if ($om = $registry->getManager($omName)) {
             return $om;
         }
@@ -115,4 +144,5 @@ class ObjectToIdTransformer implements DataTransformerInterface
 
         throw new InvalidConfigurationException(sprintf('Repository for class "%s" does not exist.', $class));
     }
+
 }
